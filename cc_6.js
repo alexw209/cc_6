3762ea7b10a9090eaed0164edacd5a184a27ba4a
Alex White <awhite6@usf.edu>
2025-10-03 22:07:41 -0400

    Created company class and listed employees

--- cc_6.js
+++ cc_6.js
@@ -22,8 +22,24 @@ class Manager extends Employee {
     }
 }
 
-// Employees and Managers
 
+// Create company class
+class Company {
+  constructor(name) {
+    this.name = name;
+    this.employees = [];
+  }
+
+  addEmployee(employee) {
+    this.employees.push(employee);
+  }
+
+  listEmployees() {
+    this.employees.forEach(e => console.log(e.describe()));
+  }
+}
+
+// Employees and Managers
 const emp1 = new Employee("Jimmy John", "Sandwich")
 const emp2 = new Employee("Papa John", "Pizza")
 const emp3 = new Employee("Colonel Sanders", "Chicken")
@@ -31,3 +47,8 @@ const emp3 = new Employee("Colonel Sanders", "Chicken")
 const man1 = new Manager("Cletus Lyle", "Finance", 20)
 const man2 = new Manager("Chad Gideon", "Athletic", 5)
 const man3 = new Manager("Gordon Hadley", "Culinary", 100)
+
+const foodTitans = new Company("Food Titans");
+[emp1, emp2, emp3, man1, man2, man3].forEach(e => foodTitans.addEmployee(e));
+
+foodTitans.listEmployees();
